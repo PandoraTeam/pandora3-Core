4ba0aa8df32f987723590dcf7d3e52094db0f611
Petr Gostin <U.238@rambler.ru>
2019-09-16 21:07:10 +0300

    methods for status, headers and cookies

--- Interfaces/ResponseInterface.php
+++ Interfaces/ResponseInterface.php
@@ -1,5 +1,6 @@
 <?php
 namespace Pandora3\Core\Interfaces;
+use Pandora3\Libs\Cookie\Cookie;
 
 /**
  * Interface ResponseInterface
@@ -10,5 +11,22 @@ interface ResponseInterface {
 	function send(): void;
 
 	function getContent(): string;
+
+	function setStatus(int $status): void;
+
+	function setHeader(string $header, string $value): void;
+
+	function removeHeader(string $header): void;
+
+	function setContent(string $content): void;
+
+	function setCookie(Cookie $cookie): void;
+
+	function removeCookie(string $name, string $path = '/', ?string $domain = null): void;
+
+	function clearCookie(
+		string $name, string $path = '/', ?string $domain = null,
+		bool $isSecure = false, bool $isHttpOnly = true
+	): void;
 
 }
